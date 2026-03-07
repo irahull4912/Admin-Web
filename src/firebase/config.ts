4db4e98a5080929af88faf19ac438e2be00f84c6
Rahul Pawar <irahull4912@gmail.com>
2026-03-07 12:01:58 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-7366027244-1a3f8",
+  "appId": "1:778678411642:web:b9dd697550df965245f6c3",
+  "apiKey": "AIzaSyAeCJugVAs6tmq_THuoH3LqBn0EK70PR2g",
+  "authDomain": "studio-7366027244-1a3f8.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "778678411642"
+};
---
+++ src/firebase/firestore/config.ts
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+    "projectId": "studio-5676793669-2a70d",
+    "appId": "1:564605611126:web:70b38e6a060538fc744357",
+    "apiKey": "AIzaSyCrd4pnTO_JjoFpdY_OUqPhet-k_8H6-ko",
+    "authDomain": "studio-5676793669-2a70d.firebaseapp.com",
+    "storageBucket" : "studio-5676793669-2a70d.firebasestorage.app",
+    "measurementId": "",
+    "messagingSenderId": "564605611126"
+  };
